06f785f57499cd0039a1ccd43c278bffe4cb3b30
Ben B <bburdette@gmail.com>
2014-12-12 22:50:26 -0700

    addsequence form

---
+++ Handler/AddSequence.hs
@@ -0,0 +1,26 @@
+module Handler.AddSequence where
+
+import Import
+
+newSequenceForm :: Form Sequence 
+newSequenceForm = renderDivs $ Sequence
+  <$> areq textField "Name" Nothing
+
+getAddSequenceR :: Handler Html
+getAddSequenceR = do 
+  (widget,enctype) <- generateFormPost newSequenceForm 
+  defaultLayout $ [whamlet|
+    <h1> Sequence
+    <form method=post enctype=#{enctype}>
+      ^{widget}
+    |]
+
+postAddSequenceR :: Handler Html
+postAddSequenceR = do 
+  ((res, projectWidget),enctype) <- runFormPost newSequenceForm
+  case res of 
+    FormSuccess newseq -> do
+      seqid <- runDB $ insert newseq 
+      redirect SequencesR
+    _ -> defaultLayout [whamlet|fale!|]
+
