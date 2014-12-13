7acb60eaec578596d9c88c38df2bb053acbda035
Ben B <bburdette@gmail.com>
2014-12-12 21:30:49 -0700

    sequences handlers.

---
+++ Handler/Sequences.hs
@@ -0,0 +1,9 @@
+module Handler.Sequences where
+
+import Import
+
+getSequencesR :: Handler Html
+getSequencesR = error "Not yet implemented: getSequencesR"
+
+postSequencesR :: Handler Html
+postSequencesR = error "Not yet implemented: postSequencesR"
