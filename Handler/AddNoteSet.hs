b4d3ce1d757a674269982158471f4581c2e82cf7
Ben B <bburdette@gmail.com>
2014-12-14 12:57:48 -0700

    chord type checkboxen

--- Handler/AddNoteSet.hs
+++ Handler/AddNoteSet.hs
@@ -1,9 +1,16 @@
 module Handler.AddNoteSet where
 
 import Import
+import Handler.NoteSet
 
 getAddNoteSetR :: Handler Html
-getAddNoteSetR = error "Not yet implemented: getAddNoteSetR"
+getAddNoteSetR = do 
+  (ncks,etype) <- generateFormPost $ noteChecks (noteArray 12) Nothing
+  defaultLayout $ [whamlet|
+    <h1> Chord type
+    <form method=post enctype=#{etype}>
+      ^{ncks} 
+    |]
 
 postAddNoteSetR :: Handler Html
 postAddNoteSetR = error "Not yet implemented: postAddNoteSetR"
--- Handler/NoteSet.hs
+++ Handler/NoteSet.hs
@@ -1,42 +1,27 @@
 module Handler.NoteSet where
 
 import Import
+import qualified Data.Text as T
 
 data NoteChecks = NoteChecks 
-  { note00 :: Bool,
-    note01 :: Bool,
-    note02 :: Bool,
-    note03 :: Bool,
-    note04 :: Bool,
-    note05 :: Bool,
-    note06 :: Bool,
-    note07 :: Bool,
-    note08 :: Bool,
-    note09 :: Bool,
-    note10 :: Bool,
-    note11 :: Bool }
+  { notes :: [Bool]
+  }
 
-noteChecks :: Maybe NoteChecks -> Form NoteChecks
-noteChecks mbnc = renderDivs $ NoteChecks
-  <$> areq checkBoxField "0" (fmap note00 mbnc) 
-  <*> areq checkBoxField "1" (fmap note01 mbnc) 
-  <*> areq checkBoxField "2" (fmap note02 mbnc) 
-  <*> areq checkBoxField "3" (fmap note03 mbnc) 
-  <*> areq checkBoxField "4" (fmap note04 mbnc) 
-  <*> areq checkBoxField "5" (fmap note05 mbnc) 
-  <*> areq checkBoxField "6" (fmap note06 mbnc) 
-  <*> areq checkBoxField "7" (fmap note07 mbnc) 
-  <*> areq checkBoxField "8" (fmap note08 mbnc) 
-  <*> areq checkBoxField "9" (fmap note09 mbnc) 
-  <*> areq checkBoxField "10" (fmap note10 mbnc) 
-  <*> areq checkBoxField "11" (fmap note11 mbnc) 
+noteArray :: Int -> [(Text,Bool)]
+noteArray count = 
+  map (\i -> (makename i, False)) [0..count]
+  where makename i = T.append (T.pack " ") (T.pack (show i))
+
+noteChecks :: [(Text,Bool)] -> Maybe NoteChecks -> Form NoteChecks
+noteChecks blah mbnc = renderDivs $ NoteChecks
+  <$> areq (checkboxesFieldList blah) "notes: " (fmap notes mbnc)
 
 noteSetForm :: Maybe NoteSet -> Form NoteSet 
 noteSetForm mbnoteSet = renderDivs $ NoteSet
   <$> areq textField "Name" (fmap noteSetName mbnoteSet)
 
 getNoteSetR :: NoteSetId -> Handler Html
-getNoteSetR = error "Not yet implemented: getNoteSetR"
-
+getNoteSetR = error "Not yet implemented: postNoteSetR"
+  
 postNoteSetR :: NoteSetId -> Handler Html
 postNoteSetR = error "Not yet implemented: postNoteSetR"
