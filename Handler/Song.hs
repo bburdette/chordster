085c82bf130995fde7dced423d479a571dc07b9f
Ben B <bburdette@gmail.com>
2014-12-15 23:28:20 -0700

    mostly working chord-add for songs.

--- Handler/Song.hs
+++ Handler/Song.hs
@@ -29,7 +29,7 @@ getSongR sid = do
     <h1> Song
     <form method=post enctype=#{enctype}>
       ^{swidget}
-      <input type=submit value="OK">
+      <input type=submit name="oksong" value="OK">
     <table class=song>
       <tr>
         <th> Sequence 
@@ -44,7 +44,7 @@ getSongR sid = do
           <td> #{show (songChordDuration songchord)}
     <form method=post enctype=#{enctype}>
       ^{scwidget}
-      <input type=submit value="add chord">
+      <input type=submit name="addchord" value="add chord">
    |]
 
 {-
@@ -56,10 +56,28 @@ getSongR sid = do
 
 postSongR :: SongId -> Handler Html 
 postSongR sid = do 
-  ((res, widget),enctype) <- runFormPost (songForm Nothing)
-  case res of 
-    FormSuccess song -> do
-      res2 <- runDB $ replace sid song
+  addc <- lookupPostParam "addchord"
+  oksong <- lookupPostParam "oksong"
+  case (oksong,addc) of 
+    (Just _, _) -> do 
+      ((res, widget),enctype) <- runFormPost (songForm Nothing)
+      case res of 
+        FormSuccess song -> do
+          res2 <- runDB $ replace sid song
+          redirect SongsR
+        _ -> defaultLayout [whamlet|fale!|]
+    (_, Just _) -> do 
+      chordz <- runDB $ selectList [SongChordSong ==. sid] [Asc SongChordSeqnum]
+      chordroots <- runDB $ selectList [] []
+      notesets <- runDB $ selectList [] [] 
+      let rootz = map (\(Entity crid cr) -> (chordRootName cr, crid)) chordroots
+          nsetz = map (\(Entity nsid ns) -> (noteSetName ns, nsid)) notesets 
+      ((res, widget),enctype) <- runFormPost $ songChordForm Nothing sid (length chordz) rootz nsetz 
+      case res of 
+        FormSuccess songchord -> do
+          res2 <- runDB $ insert songchord
+          redirect (SongR sid)
+        _ -> defaultLayout [whamlet|meh!|]
+    _ -> do
       redirect SongsR
-    _ -> defaultLayout [whamlet|fale!|]
 
