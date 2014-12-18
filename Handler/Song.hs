e126258ff93a2c8f5d092a8283dbae683ba08531
Ben B <bburdette@gmail.com>
2014-12-17 17:02:23 -0700

    Scf datatype.

--- Handler/Song.hs
+++ Handler/Song.hs
@@ -8,6 +8,45 @@ songForm mbsong = renderTable $ Song
   <$> areq textField "Name" (fmap songName mbsong)
   <*> areq intField "Tempo" (fmap songTempo mbsong)
 
+data Scf = Scf 
+  {
+    song :: SongId,
+    chordroot :: ChordRootId,
+    noteset :: NoteSetId,
+    seqnum :: Int,
+    duration :: Int,
+    scid :: Maybe SongChordId
+  } 
+
+toScf sc scid = 
+  Scf {
+    song = songChordSong sc, 
+    chordroot = songChordChordroot sc, 
+    noteset = songChordNoteset sc, 
+    seqnum = songChordSeqnum sc, 
+    duration = songChordDuration sc, 
+    scid = Just scid 
+    }
+
+fromScf scf = 
+  ((scid scf),
+   SongChord {
+    songChordSong = song scf, 
+    songChordChordroot = chordroot scf, 
+    songChordNoteset = noteset scf, 
+    songChordSeqnum = seqnum scf, 
+    songChordDuration = duration scf
+   })
+  
+scfFrm :: Maybe Scf -> SongId -> Int -> [(Text,Key ChordRoot)] -> [(Text,Key NoteSet)] -> Form Scf
+scfFrm mbscf sid seqnum chordroots notesets = renderTable $ Scf 
+  <$> pure (maybe sid song mbscf)
+  <*> areq (selectFieldList chordroots) "Root Note" (chordroot <$> mbscf)
+  <*> areq (selectFieldList notesets) "Chord Type" (noteset <$> mbscf)
+  <*> pure seqnum
+  <*> areq intField "Duration (beats)" (duration <$> mbscf)
+  <*> pure (maybe Nothing scid mbscf)
+
 songChordForm :: Maybe SongChord -> SongId -> Int -> [(Text,Key ChordRoot)] -> [(Text,Key NoteSet)] -> Form SongChord
 songChordForm mbsc sid seqnum chordroots notesets = renderTable $ SongChord 
   <$> pure (maybe sid songChordSong mbsc)
@@ -40,7 +79,7 @@ getSongR sid = do
     $forall (scid, (widget,etype)) <- chordforms
       <form method=post enctype=#{etype}>
         ^{widget}
-        <input type=submit name=#{show scid} value="ed chord">
+        <input type=submit name="updated" value="ed chord">
     <form method=post enctype=#{enctype}>
       ^{scwidget}
       <input type=submit name="addchord" value="add chord">
@@ -64,7 +103,8 @@ postSongR sid = do
       notesets <- runDB $ selectList [] [] 
       let rootz = map (\(Entity crid cr) -> (chordRootName cr, crid)) chordroots
           nsetz = map (\(Entity nsid ns) -> (noteSetName ns, nsid)) notesets 
-      ((res, widget),enctype) <- runFormPost $ songChordForm Nothing sid (length chordz) rootz nsetz 
+      ((res, widget),enctype) <- 
+        runFormPost $ songChordForm Nothing sid (length chordz) rootz nsetz 
       case res of 
         FormSuccess songchord -> do
           res2 <- runDB $ insert songchord
