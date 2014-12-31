e41200cbfd07d789c987f9010cc0d9a7570ba247
Ben B <bburdette@gmail.com>
2014-12-31 10:21:16 -0700

    some halfway attempts at text export

--- Handler/ExportSong.hs
+++ Handler/ExportSong.hs
@@ -4,9 +4,31 @@ import Import
 import PlaySong
 import Text.Show.Pretty
 
+--getExportSongR :: SongId -> Handler Import.Html
 getExportSongR :: SongId -> Handler Import.Html
 getExportSongR sid = do 
   mbtextsong <- loadTextSong sid   
   case mbtextsong of 
-    Just ts -> defaultLayout $ [whamlet|#{ppShow ts}|]
-    Nothing -> defaultLayout $ [whamlet|not found!|]
+    Just ts -> defaultLayout [whamlet|#{ppShow ts}|]
+    Nothing -> defaultLayout [whamlet|not found!|]
+
+{-
+getExportSongR :: SongId -> Handler Text
+getExportSongR sid = do 
+  mbtextsong <- loadTextSong sid   
+  case mbtextsong of 
+    Just ts -> renderHtml [whamlet|#{ppShow ts}|]
+    Nothing -> renderHtml [whamlet|not found!|]
+
+-}
+
+{-
+textLayout :: WidgetT site IO () -> HandlerT site IO Import.Html
+textLayout w = do
+    p <- widgetToPageContent w
+    withUrlRenderer [hamlet|
+        $newline never
+        $doctype 5
+        ^{pageBody p}
+        |]
+-}
