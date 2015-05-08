e81884b361f8a20b8896c30cb77c2d1555c8592f
Ben B <bburdette@gmail.com>
2015-05-08 11:16:08 -0600

    plain text export

--- Handler/ExportSong.hs
+++ Handler/ExportSong.hs
@@ -3,32 +3,13 @@ module Handler.ExportSong where
 import Import
 import PlaySong
 import Text.Show.Pretty
+import qualified Data.Text as T
 
---getExportSongR :: SongId -> Handler Import.Html
-getExportSongR :: SongId -> Handler Import.Html
+getExportSongR :: SongId -> Handler RepPlain 
 getExportSongR sid = do 
   mbtextsong <- loadTextSong sid   
   case mbtextsong of 
-    Just ts -> defaultLayout [whamlet|#{ppShow ts}|]
-    Nothing -> defaultLayout [whamlet|not found!|]
+    Just ts -> return $ RepPlain $ toContent $ T.pack (ppShow ts)
+    Nothing -> return $ RepPlain $ toContent $ T.pack "song not found"
 
-{-
-getExportSongR :: SongId -> Handler Text
-getExportSongR sid = do 
-  mbtextsong <- loadTextSong sid   
-  case mbtextsong of 
-    Just ts -> renderHtml [whamlet|#{ppShow ts}|]
-    Nothing -> renderHtml [whamlet|not found!|]
 
--}
-
-{-
-textLayout :: WidgetT site IO () -> HandlerT site IO Import.Html
-textLayout w = do
-    p <- widgetToPageContent w
-    withUrlRenderer [hamlet|
-        $newline never
-        $doctype 5
-        ^{pageBody p}
-        |]
--}
