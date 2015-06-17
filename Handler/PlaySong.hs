275393df4f9dbc0df0c9d61f559ddc8950937386
Ben B <bburdette@gmail.com>
2015-06-17 15:52:43 -0600

    clean up the code a bit.

--- Handler/PlaySong.hs
+++ Handler/PlaySong.hs
@@ -35,36 +35,26 @@ playSongWs sid = do
       let websong = toWebSong song chords
           wsjs = toJSON websong
       sendTextData (toJsonText wsjs)
-      -- (liftIO . atomically) $ writeTChan textchan (toJsonText wsjs)
       return () 
     (Just (song, chords), Just (tid, _)) -> do  
       -- kill the old song thread. 
       liftIO $ print $ "killing old thread: " ++ show tid
       liftIO $ TR.mapM (\(tid,_) -> killThread tid) oldinfo
       -- start a new song thread
-      threadid <- lift $ startSongThread' writeChan song chords 
-      -- threadid <- liftIO $ forkIO $ 
-      --   playSong writeChan song chords chordips lightips
+      threadid <- lift $ startSongThread writeChan song chords 
       -- save the new thread id and song id in the MVar.
       _ <- liftIO $ tryTakeMVar $ playThread $ songControl app
       res <- liftIO $ putMVar (playThread $ songControl app) (threadid, sid)
       liftIO $ print $ "res= " ++ show res
-      {-
-      -}
       return ()
     (Just (song, chords), Nothing) -> do 
       liftIO $ print $ "starting new song thread"  
       -- start a new song thread
-      threadid <- lift $ startSongThread' writeChan song chords 
-      -- store in the mvar.
+      threadid <- lift $ startSongThread writeChan song chords 
+      -- save the new thread id and song id in the MVar.
       res <- liftIO $ putMVar (playThread $ songControl app) (threadid, sid)
       liftIO $ print $ "nothing res= " ++ show res
-      {-
-      threadid <- liftIO $ forkIO $ 
-        playSong (songLine app) song (catMaybes pscs) chordips lightips
-      -}
       return ()
-  -- lift $ startSongThread sid
   liftIO $ print "pre readTChan, etc"
   (forever $ (liftIO . atomically) (readTChan readChan) >>= sendTextData)
   liftIO $ print "post readTChan, etc"
@@ -93,11 +83,8 @@ getSongInfo sid = do
     (Just song) -> return (Just (song, catMaybes songchords))
     Nothing -> return Nothing
 
--- playSong :: TChan Text -> Song -> [PlaySongChord] -> [(String,Int)] -> [(String,Int)] -> IO ()
--- playSong textchan song chords chorddests lightdests = do
-
-startSongThread' :: TChan Text -> Song -> [PlaySongChord] -> Handler ThreadId
-startSongThread' textchan song chords = do
+startSongThread :: TChan Text -> Song -> [PlaySongChord] -> Handler ThreadId
+startSongThread textchan song chords = do
   chorddests <- runDB $ selectList [OSCDestType ==. T.pack "chords"] [] 
   lightdests <- runDB $ selectList [OSCDestType ==. T.pack "lights"] [] 
   let chordips = map (\(Entity _ dest) -> 
@@ -108,86 +95,7 @@ startSongThread' textchan song chords = do
                       lightdests
   liftIO $ forkIO $ 
     playSong textchan song chords chordips lightips
-  {-
-  -- kill the old song, if any.
-  oldinfo <- liftIO $ tryReadMVar $ playThread $ songControl app
-  liftIO $ print $ "oldinfo: " ++ show oldinfo
-  case oldinfo of 
-    Just (tid, id) | id == sid -> do  
-      liftIO $ print $ "song thread exists; doing nothing. " ++ show (tid, id)
-      return () 
-    Just (tid, _) -> do
-      -- kill the old song thread. 
-      liftIO $ print $ "killing old thread: " ++ show tid
-      lift $ TR.mapM (\(tid,_) -> killThread tid) oldinfo
-      -- start a new song thread
-      threadid <- liftIO $ forkIO $ 
-        playSong (songLine app) song (catMaybes pscs) chordips lightips
-      -- save the new thread id and song id in the MVar.
-      _ <- liftIO $ tryTakeMVar $ playThread $ songControl app
-      res <- liftIO $ putMVar (playThread $ songControl app) (threadid, sid)
-      liftIO $ print $ "res= " ++ show res
-      return ()
-    Nothing -> do
-      liftIO $ print $ "starting new song thread"  
-      -- start a new song thread
-      threadid <- liftIO $ forkIO $ 
-        playSong (songLine app) song (catMaybes pscs) chordips lightips
-      -- store in the mvar.
-      res <- liftIO $ putMVar (playThread $ songControl app) (threadid, sid)
-      liftIO $ print $ "nothing res= " ++ show res
-      return ()
- -}
 
-startSongThread :: SongId -> Handler ()
-startSongThread sid = do
-  app <- getYesod 
-  mbsong <- runDB $ get sid
-  chords <- runDB $ selectList [SongChordSong ==. sid] [Asc SongChordSeqnum]
-  chorddests <- runDB $ selectList [OSCDestType ==. T.pack "chords"] [] 
-  lightdests <- runDB $ selectList [OSCDestType ==. T.pack "lights"] [] 
-  case (mbsong,chorddests) of 
-    (Nothing,_) -> 
-      error "song not found"
-    (_,[]) ->
-      error "no osc IP destinations set up"
-    (Just song, dests) -> do
-      let chordips = map (\(Entity _ dest) -> 
-                            (T.unpack $ oSCDestIp dest, oSCDestPort dest)) 
-                          chorddests
-          lightips = map (\(Entity _ dest) -> 
-                            (T.unpack $ oSCDestIp dest, oSCDestPort dest)) 
-                          lightdests
-      pscs <- makePscs (map entityVal chords)
-      -- kill the old song, if any.
-      oldinfo <- liftIO $ tryReadMVar $ playThread $ songControl app
-      liftIO $ print $ "oldinfo: " ++ show oldinfo
-      case oldinfo of 
-        Just (tid, id) | id == sid -> do  
-          liftIO $ print $ "song thread exists; doing nothing. " ++ show (tid, id)
-          return () 
-        Just (tid, _) -> do
-          -- kill the old song thread. 
-          liftIO $ print $ "killing old thread: " ++ show tid
-          lift $ TR.mapM (\(tid,_) -> killThread tid) oldinfo
-          -- start a new song thread
-          threadid <- liftIO $ forkIO $ 
-            playSong (songLine app) song (catMaybes pscs) chordips lightips
-          -- save the new thread id and song id in the MVar.
-          _ <- liftIO $ tryTakeMVar $ playThread $ songControl app
-          res <- liftIO $ putMVar (playThread $ songControl app) (threadid, sid)
-          liftIO $ print $ "res= " ++ show res
-          return ()
-        Nothing -> do
-          liftIO $ print $ "starting new song thread"  
-          -- start a new song thread
-          threadid <- liftIO $ forkIO $ 
-            playSong (songLine app) song (catMaybes pscs) chordips lightips
-          -- store in the mvar.
-          res <- liftIO $ putMVar (playThread $ songControl app) (threadid, sid)
-          liftIO $ print $ "nothing res= " ++ show res
-          return ()
- 
 postPlaySongR :: SongId -> Handler Html
 postPlaySongR sid = do 
   meh <- lookupPostParam "stop"
@@ -201,5 +109,3 @@ postPlaySongR sid = do
          Nothing -> return ()
          Just (tid,_) -> lift $ killThread tid 
       redirect SongsR
-
-
