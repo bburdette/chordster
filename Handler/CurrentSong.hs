7f5fbff0254f3a54b49504386c839daad0e96838
Ben B <bburdette@gmail.com>
2015-07-09 13:11:29 -0600

    remove dead code

--- Handler/CurrentSong.hs
+++ Handler/CurrentSong.hs
@@ -12,13 +12,6 @@ import PlayWhatever
 getCurrentSongR :: Handler Html
 getCurrentSongR = do 
   app <- getYesod 
-  {-
-  wat <- liftIO $ readIORef $ whateverThread $ songControl app
-  case wat of 
-    Just (_,WhatSong songid) -> redirect $ PlaySongR songid
-    Just (_,WhatSongSequence songseqid) -> redirect $ PlaySongSequenceR songseqid
-    Nothing -> do  
-  -}
   -- is there a current song?
   mbsid <- liftIO $ readIORef ((currentSong . songControl) app)
   case mbsid of 
